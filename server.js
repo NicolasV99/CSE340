b8b606b8563004432039e586ede6e8da2676fdf2
NicolasV99 <legolasnicolas99@gmail.com>
2025-01-20 20:24:22 -0500

    add MVC and show inventary with header nav items from database with SQL querys

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+const baseController = require("./controllers/baseController")
+
+const inventoryRoute = require("./routes/inventoryRoute")
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -26,9 +30,14 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //Index route
-app.get("/", function(req, res){
+/*app.get("/", function(req, res){
   res.render("index", {title: "Home"})
-})
+})*/
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information
